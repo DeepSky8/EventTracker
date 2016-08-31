e6d60392b19ac9ee52c27157b1a237ed6d02b5d8
Joel Jackson <DeepSky8@gmail.com>
2016-08-31 16:26:06 -0400

    Integrated Owin and Entity Framework successfully.

--- RSVP/Domain/Model/Users.cs
+++ RSVP/Domain/Model/Users.cs
@@ -8,22 +8,23 @@ namespace RSVP.Domain.Model
 {
     public class Users
     {
+        [Key]
         public int UserId { get; set; }
 
-        [Required]
-        [Display(Name = "User name")]
+        //[Required]
+        //[Display(Name = "User name")]
         public string UserName { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Password")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        //public string ConfirmPassword { get; set; }
 
 
         public string EmailAddress { get; set; }
